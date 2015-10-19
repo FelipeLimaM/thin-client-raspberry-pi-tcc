451fd45e1a7416e5f8a6596de1be6f5448f539d9
Felipe Lima Morais <FelipeLimaM@users.noreply.github.com>
2015-10-19 16:59:16 -0300

    Create run_cpu_data_collect.sh

---
+++ Thin Client/Teste Basico/run_cpu_data_collect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+file='cpu'
+
+while (cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')>>file; do 
+    i=0; 
+done
